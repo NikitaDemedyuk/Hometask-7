ec82c00e28d55872a72513d777f0832cabaeef0b
nikita <nikitademedyuk1@gmail.com>
2020-11-24 12:27:16 +0300

    add

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+//import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter the length of array:");
+            int lengthArray = in.nextInt();
+            List<Integer> array = createRandomArray(lengthArray);
+            System.out.print("Do you want to see your array?\n1 - See\n2 - Skip");
+            int variantSee = in.nextInt();
+            switch (variantSee) {
+                case 1:
+                    printArray(array);
+            }
+
+            System.out.println("Sort array:\n1 - Sort in descending order (by value)\n2 - Sort in ascending order(by value)\n3 - Sort in descending order (by number of elements)\n4 - Sort in ascending order(by number of elements)");
+            int typeSort = in.nextInt();
+
+            TypeOfSort sortArray = new TypeOfSort(typeSort);
+            sortArray.start();
+            sortArray.run(array);
+
+        }
+    }
+
+    public static List<Integer> createRandomArray(int lengthArray) {
+        List<Integer> array = new ArrayList<>();
+        for (int i = 0; i < lengthArray; i++) {
+            array.add((int) (Math.random() * 30) - 15);
+        }
+        return array;
+    }
+
+    public static void printArray (List <Integer> array) {
+        System.out.println("Your array:");
+        for (int i = 0; i < array.size(); ++i) {
+            System.out.print(array.get(i) + " ");
+        }
+    }
+            /*public class Main extends Thread {
+                private int sizeArray;
+                private int typeOfSort;
+
+                Main() {
+                    sizeArray = 0;
+                    typeOfSort = 1;
+                }
+
+                Main(int sizeArrayNow, int typeOfSortNow) {
+                    sizeArray = sizeArrayNow;
+                    typeOfSort = typeOfSortNow;
+                }
+
+                @Override
+                public void run() {
+                    System.out.println("thread main is running");
+                    List<Integer> randomArray = new ArrayList<>();
+                    for (int i = 0; i < sizeArray; i++) {
+                        randomArray.add((int) (Math.random() * 30) - 15);
+                    }
+                    for (int i = 0; i < randomArray.size(); ++i) {
+                        System.out.print(randomArray.get(i) + " ");
+                    }
+
+                    TypeOfSort sort = new TypeOfSort(typeOfSort);
+                    sort.run(randomArray);
+                    System.out.println();
+                    for (int i : randomArray) {
+                        System.out.print(i + " ");
+                    }
+                }
+            }
+          */
+}
+
+
---
+++ src/main/java/com/company/TypeOfSort.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.*;
+import static java.sql.Types.NULL;
+//import javafx.application.Application;
+
+
+public class TypeOfSort extends Thread{
+    private int typeSort;
+
+    TypeOfSort(int variant) {
+        typeSort = variant;
+    }
+
+    public void run(List<Integer> randomArray) {
+        Scanner in = new Scanner(System.in);
+
+        switch (typeSort) {
+            case 1:
+                Comparator comparator1 = new CompareByValue();
+                Collections.sort(randomArray, comparator1);
+            case 2:
+                Comparator comparator2 = new CompareByValue();
+                List<Integer> randomArrayInverse1 = new ArrayList<>();
+                Collections.sort(randomArrayInverse1, comparator2);
+                for (int i = 0; i < randomArray.size(); ++i) {
+                    randomArray.set(i, randomArrayInverse1.get(randomArray.size() - i - 1));
+                }
+            case 3:
+                Comparator comparator3 = new CompareByNumberOfElements();
+                Collections.sort(randomArray, comparator3);
+            case 4:
+                Comparator comparator4 = new CompareByNumberOfElements();
+                List<Integer> randomArrayInverse2 = new ArrayList<>();
+                Collections.sort(randomArrayInverse2, comparator4);
+                for (int i = 0; i < randomArray.size(); i++) {
+                    randomArray.set(i, randomArrayInverse2.get(randomArray.size() - i - 1));
+                }
+        }
+    }
+}
+
+
+
